cc19ead39648fd457038f8008e6f2dfac1c386a1
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-09-30 20:12:41 +0300

    Solved the task.

--- cashier.py
+++ cashier.py
@@ -11,14 +11,23 @@ def get_invoice_items(items):
     # ['1 Apple 0.200KD', '4 Orange 1.200KD']
     # ---
     # Write your code here
-    ...
+    
+    invoice_items = []
+    for item in items:
+        total = format(item['price'] * item['quantity'], '.3f')
+        formatted_item = f"{round(item['quantity'])} {item['name']} {total}KD"
+        invoice_items.append(formatted_item)
+    return invoice_items
 
 
 def get_total(items):
     # Items is a dictionary with a quantity and price key
     # Calculate the total of all items in the cart
     # Write your code here
-    ...
+    total = 0
+    for item in items:
+        total += item['quantity'] * item['price']
+    return format(total, '.3f')
 
 
 def print_receipt(invoice_items, total):
@@ -28,12 +37,32 @@ def print_receipt(invoice_items, total):
     # total at the end.
     # ---
     # Write your code here
-    ...
+    print("-------------------")
+    print("receipt")
+    print("-------------------")
+    for item in invoice_items:
+        print(item)
+    print("-------------------")
+    print(f"Total Price: {total}KD")
 
 
 def main():
     # Write your main logic here
-    ...
+    items = []
+    item = {}
+
+    item["name"] = input('Item (enter "done" when finished): ')
+    while item["name"] != "done":
+        item["price"] = float(input("Price: "))
+        item["quantity"] = float(input("Quantity: "))
+        items.append(item)
+        item = {}
+        item["name"] = input('Item (enter "done" when finished): ')
+    
+    invoice_items = get_invoice_items(items)
+    total = get_total(items)
+    print_receipt(invoice_items, total)
+
 
 
 if __name__ == "__main__":
